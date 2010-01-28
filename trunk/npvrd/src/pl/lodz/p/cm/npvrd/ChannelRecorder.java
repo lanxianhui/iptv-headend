c8a72f180e650d9ed54b055a6098f503d9a3a470
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-01-28 22:04:34 +0000

    ChannelRecorder powinien już działać.

--- trunk/npvrd/src/pl/lodz/p/cm/npvrd/ChannelRecorder.java
+++ trunk/npvrd/src/pl/lodz/p/cm/npvrd/ChannelRecorder.java
@@ -3,7 +3,9 @@ package pl.lodz.p.cm.npvrd;
 import java.io.*;
 import java.net.*;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class ChannelRecorder implements Runnable {
 	
@@ -33,7 +35,7 @@ public class ChannelRecorder implements Runnable {
 		this.groupPort = groupPort;
 	}
 	
-	private String generateFileName(String programTitle, Date poczatekNagrywania, Date koniecNagrywania) {
+	private String generateFileName(RecordingTask task) {
 		return "";
 	}
 
@@ -44,43 +46,51 @@ public class ChannelRecorder implements Runnable {
 			MulticastSocket sock = new MulticastSocket(groupPort);
 			sock.joinGroup(group);
 			
-			try {
-				FileOutputStream fos = new FileOutputStream(fileName);
-				
-				byte[] buf = new byte[sock.getReceiveBufferSize()];
-				DatagramPacket recv = new DatagramPacket(buf, buf.length);
-				
-				long poczatekNagrywaniaLiczb = poczatekNagrywania.getTime();
-				long koniecNagrywaniaLiczb = koniecNagrywania.getTime();
-				
-				System.out.println(fileName + ": Czekam na: " + poczatekNagrywania.toGMTString());
-				
-				while (System.currentTimeMillis() < poczatekNagrywaniaLiczb)
-				{
-					sock.receive(recv);
+			
+			while (!programNagrywania.isEmpty())
+			{
+				try {
+					RecordingTask task = programNagrywania.removeLast();
+					String fileName = generateFileName(task);
+					
+					FileOutputStream fos = new FileOutputStream(fileName);
+					
+					byte[] buf = new byte[sock.getReceiveBufferSize()];
+					DatagramPacket recv = new DatagramPacket(buf, buf.length);
+					
+					long poczatekNagrywaniaLiczb = task.getPoczatekNagrywania().getTime();
+					long koniecNagrywaniaLiczb = task.getKoniecNagrywania().getTime();
+					
+					System.out.println(fileName + ": Czekam na: " + task.getPoczatekNagrywania().toGMTString());
+					
+					while (System.currentTimeMillis() < poczatekNagrywaniaLiczb)
+					{
+						sock.receive(recv);
+					}
+					
+					System.out.println(groupIp + ": Rozpoczynam nagrywanie");
+					
+					while (System.currentTimeMillis() < koniecNagrywaniaLiczb)
+					{
+						sock.receive(recv);
+						fos.write(recv.getData(), 0, recv.getLength());
+					}
+					
+					System.out.println(groupIp + ": Jest już: " + task.getPoczatekNagrywania().toGMTString());			
+					System.out.println(groupIp + ": Koniec nagrywania");
+					
+					fos.close();
+				} catch (IOException e) {
+					System.err.println(groupIp + ": Plik docelowy jest niedostępny");
 				}
-				
-				System.out.println(fileName + ": Rozpoczynam nagrywanie");
-				
-				while (System.currentTimeMillis() < koniecNagrywaniaLiczb)
-				{
-					sock.receive(recv);
-					fos.write(recv.getData(), 0, recv.getLength());
-				}
-				
-				System.out.println(fileName + ": Jest już: " + koniecNagrywania.toGMTString());			
-				System.out.println(fileName + ": Koniec nagrywania");
-				
-				fos.close();
-			} catch (IOException e) {
-				System.err.println(fileName + ": Plik docelowy jest niedostępny");
 			}
+			
 			sock.leaveGroup(group);
 			sock.close();
 		} catch (UnknownHostException e) {
-			System.err.println(fileName + ": Błędnie podany adres nasłuchowy");
+			System.err.println(groupIp + ": Błędnie podany adres nasłuchowy");
 		} catch (IOException e) {
-			System.err.println(fileName + ": Port nasłuchowy zajęty");
+			System.err.println(groupIp + ": Port nasłuchowy zajęty");
 		}
 	}
 
