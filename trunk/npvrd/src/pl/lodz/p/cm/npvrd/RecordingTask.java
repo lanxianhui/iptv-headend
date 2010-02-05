d0d117b9fd5b9cba670914128221a11cc55223b9
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-02-05 21:02:48 +0000

    More comments in the code

--- trunk/npvrd/src/pl/lodz/p/cm/npvrd/RecordingTask.java
+++ trunk/npvrd/src/pl/lodz/p/cm/npvrd/RecordingTask.java
@@ -2,17 +2,17 @@ package pl.lodz.p.cm.npvrd;
 
 import java.util.Date;
 
-public class RecordingTask {
+public class RecordingTask implements Comparable<RecordingTask> {
 	
 	private String programName;
 	private Date recordingBegin;
 	private Date recordingEnd;
 	
 	/**
-	 * Represents a single program to be recorded
-	 * @param programName Program to be recorded
-	 * @param recordingBegin Program's start time 
-	 * @param koniecNagrywania Program's end time
+	 * Represents a single program to be recorded.
+	 * @param programName Program to be recorded.
+	 * @param recordingBegin Program's start time. 
+	 * @param koniecNagrywania Program's end time.
 	 */
 	public RecordingTask(String programName, Date recordingBegin, Date recordingEnd) {
 		this.programName = programName;
@@ -21,17 +21,17 @@ public class RecordingTask {
 	}
 	
 	/**
-	 * Creates a new recording task based on an old one
-	 * @param oldTask Old task to be copied
+	 * Creates a new recording task based on an old one.
+	 * @param oldTask Old task to be copied.
 	 */
 	public RecordingTask(RecordingTask oldTask) {
-		this.programName = oldTask.getProgramName();
-		this.recordingBegin = oldTask.getRecordingBegin();
-		this.recordingEnd = oldTask.getRecordingEnd();
+		this.programName = oldTask.programName;
+		this.recordingBegin = oldTask.recordingBegin;
+		this.recordingEnd = oldTask.recordingEnd;
 	}
 
 	/**
-	 * Get task program name
+	 * Get task program name.
 	 * @return
 	 */
 	public String getProgramName() {
@@ -56,6 +56,12 @@ public class RecordingTask {
 
 	public void setRecordingEnd(Date koniecNagrywania) {
 		this.recordingEnd = koniecNagrywania;
+	}
+
+	@Override
+	public int compareTo(RecordingTask o) {
+		// TODO Auto-generated method stub
+		return this.recordingBegin.compareTo(o.recordingBegin);
 	}
 
 }
