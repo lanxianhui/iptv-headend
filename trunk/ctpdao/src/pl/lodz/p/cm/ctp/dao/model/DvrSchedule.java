48601619cf197a56bc9e1f018c377ee0128eb72c
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-02-13 18:00:30 +0000

    Model classes for DvrSchedule and TvChannel.

---
+++ trunk/ctpdao/src/pl/lodz/p/cm/ctp/dao/model/DvrSchedule.java
@@ -0,0 +1,71 @@
+package pl.lodz.p.cm.ctp.dao.model;
+
+public class DvrSchedule {
+	
+	public enum Mode { WAITING, PROCESSING, AVAILABLE, UNAVAILABLE };
+
+	private Long id;
+	private Long programId;
+	private Mode mode;
+	private String fileName;
+	
+	public DvrSchedule() {
+		
+	}
+	
+	public DvrSchedule(Long id, Long programId) {
+		this.id = id;
+		this.programId = programId;
+	}
+	
+	public DvrSchedule(Long id, Long programId, Mode mode, String fileName) {
+		this(id, programId);
+		this.mode = mode;
+		this.fileName = fileName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getProgramId() {
+		return programId;
+	}
+
+	public void setProgramId(Long programId) {
+		this.programId = programId;
+	}
+
+	public Mode getMode() {
+		return mode;
+	}
+
+	public void setMode(Mode mode) {
+		this.mode = mode;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public boolean equals(Object other) {
+		return (other instanceof DvrSchedule) && (id != null) ? id.equals(((DvrSchedule) other).id) : (other == this);
+	}
+	
+	public int hashCode() {
+        return (id != null) ? (this.getClass().hashCode() + id.hashCode()) : super.hashCode();
+    }
+	
+	public String toString() {
+		return String.format("DvrSchedule[id=%d,programId=%d,mode=%s,fileName=%s]", 
+	            id, programId, mode.toString(), fileName);
+	}
+}
--- trunk/ctpdao/src/pl/lodz/p/cm/ctp/dao/model/TvChannel.java
+++ trunk/ctpdao/src/pl/lodz/p/cm/ctp/dao/model/TvChannel.java
@@ -2,4 +2,78 @@ package pl.lodz.p.cm.ctp.dao.model;
 
 public class TvChannel {
 
+	private Long id;
+	private String name;
+	private String ipAdress;
+	private Integer port;
+	private String icon;
+	
+	public TvChannel() {
+		
+	}
+	
+	public TvChannel(Long id, String name, String ipAdress, Integer port) {
+		this.id = id;
+		this.name = name;
+		this.ipAdress = ipAdress;
+		this.port = port;
+	}
+	
+	public TvChannel(Long id, String name, String ipAdress, Integer port, String icon) {
+		this(id, name, ipAdress, port);
+		this.icon = icon;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIpAdress() {
+		return ipAdress;
+	}
+
+	public void setIpAdress(String ipAdress) {
+		this.ipAdress = ipAdress;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	
+	public boolean equals(Object other) {
+		return (other instanceof TvChannel) && (id != null) ? id.equals(((TvChannel) other).id) : (other == this);
+	}
+	
+	public int hashCode() {
+        return (id != null) ? (this.getClass().hashCode() + id.hashCode()) : super.hashCode();
+    }
+
+	public String toString() {
+		return String.format("TvChannel[id=%d,name=%s,ipAdress=%s,port=%d,icon=%s]", 
+	            id, name, ipAdress, port, icon);
+	}
 }
