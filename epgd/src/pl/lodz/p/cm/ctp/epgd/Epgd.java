84234a80b72b86820f4b96aaea28541608c39e49
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-02-15 16:19:16 +0000

    A new module of the system, created to import XMLTV data into the database in regular intervals (also refresh the XMLTV data, etc.)

---
+++ epgd/src/pl/lodz/p/cm/ctp/epgd/Epgd.java
@@ -0,0 +1,40 @@
+package pl.lodz.p.cm.ctp.epgd;
+
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+
+public class Epgd implements Daemon {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(DaemonContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
