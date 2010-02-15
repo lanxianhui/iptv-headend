0b39e06c7a29e3fe49dd9b5add0bd90eff8b3720
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-02-15 17:06:22 +0000

    Mapping file to map XMLTV id's to database channel id's.

--- trunk/epgd/src/pl/lodz/p/cm/ctp/epgd/XmlTvGrabberConfig.java
+++ trunk/epgd/src/pl/lodz/p/cm/ctp/epgd/XmlTvGrabberConfig.java
@@ -2,12 +2,13 @@ package pl.lodz.p.cm.ctp.epgd;
 
 import java.io.Serializable;
 
-public class XmlTvGrabber implements Serializable {
+public class XmlTvGrabberConfig implements Serializable {
 
 	private static final long serialVersionUID = 4224614237610436092L;
 	public String commandLine;
 	public String arguments;
 	public String resultFile;
+	public String mapFile;
 	
 	public String toString() {
 		return commandLine + " " + arguments + " , " + resultFile;
