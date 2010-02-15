73ae2d4c0b5c747f6059b5f7e48e22bd065ed663
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-02-15 21:35:07 +0000

    Resigned from using Commons Daemon - jsvc is buggy, we will be using a shell script to run it.

--- epgd/src/pl/lodz/p/cm/ctp/epgd/XmlTvGrabberConfig.java
+++ epgd/src/pl/lodz/p/cm/ctp/epgd/XmlTvGrabberConfig.java
@@ -6,12 +6,11 @@ public class XmlTvGrabberConfig implements Serializable {
 
 	private static final long serialVersionUID = 4224614237610436092L;
 	public String commandLine;
-	public String arguments;
 	public String resultFile;
 	public String mapFile;
 	
 	public String toString() {
-		return commandLine + " " + arguments + " , " + resultFile;
+		return commandLine + " , " + resultFile;
 	}
 
 }
