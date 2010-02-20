199b3291fa45d446eb9dbd9241a769aea7aa0078
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-02-20 16:44:03 +0000

    Some changes in Recording Model and PvrLogic

---
+++ trunk/ctpweb/config.php
@@ -0,0 +1,10 @@
+<?php
+	$config["database"]["host"] = "localhost";
+	$config["database"]["name"] = "cm_iptv";
+	$config["database"]["username"] = "iptv-test";
+	$config["database"]["password"] = "iptv-test";
+	
+	$config["pvr"]["url"] = "rtsp://localhost/pvr/";
+	$config["pvr"]["downloadUrl"] = "http://localhost/ctpweb/pvr-download/";
+	$config["pvr"]["userQuota"] = 600; // 10h of recording by default
+?>
