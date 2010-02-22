3f5b22d7f5692ab74d03dd74b915f0bfdee69671
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-02-22 20:40:12 +0000

    Working on making templates work.

--- trunk/ctpweb/index.php
+++ trunk/ctpweb/index.php
@@ -4,6 +4,10 @@
 	require_once("control/EpgController.php");
 	
 	$smarty = new Smarty();
+	$smarty->template_dir = "./tpl";
+	$smarty->compile_dir = "./tpl_c";
+	$smarty->cache_dir = "./cache";
+	
 	$controller = new EpgController($config, $smarty);
 
 ?>
