0de6565a96ff5fad246160e85b97acd1bac16870
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-04-17 17:41:56 +0000

    New part of the package: NullGuide - a null XMLTV generator, can be used to generate dummy "filler" listings, for channels for which guide data is unavailable.

---
+++ nullguide/src/pl/lodz/p/cm/ctp/nullguide/NullGuide.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.cm.ctp.nullguide;
+
+public class NullGuide {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
