049b09fbfd3375a7605256a16323fe258c463dc9
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-04-21 07:18:32 +0000

    Refactoring of the code, some classes moved to common.js, improvements in Media class code, should now work nice with Internet Explorer.

--- trunk/ctpweb/js/common.js
+++ trunk/ctpweb/js/common.js
@@ -1,5 +1,7 @@
 var notificationBox;
-var notification;
+var notificationText;
+
+var openContextMenu = null;
 
 function getPageY(oElement) {
 	var iReturnValue = 0;
@@ -19,28 +21,121 @@ function getPageX(oElement) {
 	return iReturnValue;
 }
 
-function showNotification(text) {
+function notificationShow(text) {
+	notificationText.innerHTML = text;
+	
 	if (notificationBox.hasClass('hidden'))
 		notificationBox.removeClass('hidden');
-	
-	notification.innerHTML = text;
 }
 
-function hideNotification() {
+function notificationHide() {
 	if (!notificationBox.hasClass('hidden'))
 		notificationBox.addClass('hidden');
 }
 
-function bootScripts() {
+
+function ContextMenu(element, elementWidth, elementHeight) {
+	this.menuElement = null;
+	
+	this.attachmentElement = typeof(element) != 'undefined' ? element : null;
+	this.elementWidth = typeof(elementWidth) != 'undefined' ? elementWidth : 18;
+	this.elementHeight = typeof(elementHeight) != 'undefined' ? elementHeight : 23;
+	
+	this.show = function(x, y) {
+		if (this.attachmentElement != null) {
+			this.menuElement.style.left = (getPageX(this.attachmentElement) - (150 - this.elementWidth)) + "px";
+			this.menuElement.style.top = (getPageY(this.attachmentElement) + this.elementHeight) + "px";
+		}
+		else {
+			this.menuElement.style.left = x + "px";
+			this.menuElement.style.top = y + "px";
+		}
+		
+		if (this.menuElement.hasClass('hidden')) {
+			this.menuElement.removeClass('hidden');
+		}
+		
+		openContextMenu = this;
+		
+		var bodyElement = $(document.body);
+		bodyElement.addEvent('click', function(event) {
+			if (openContextMenu != null) {
+				openContextMenu.hide();
+				event.stop();
+			}
+		});
+	};
+	
+	this.hide = function() {
+		bodyElement.removeEvent('click', openContextMenu.hide);
+		
+		if (!openContextMenu.menuElement.hasClass('hidden')) {
+			openContextMenu.menuElement.addClass('hidden');
+		}
+		
+		openContextMenu.dispose();
+		openContextMenu = null;
+	};
+	
+	this.addMenuItem = function(caption, callback, isDefault) {
+		isDefault = typeof(isDefault) != 'undefined' ? isDefault : false;
+		
+		if (isDefault) {
+			extraClass = ' default';
+		}
+		else {
+			extraClass = '';
+		}
+		
+		newMenuItem = new Element('div', {
+			'class': 'menuItem' + extraClass
+		});
+		
+		newMenuItem.innerHTML = caption;
+		
+		newMenuItem.addEvent('click', callback);
+		
+		this.menuElement.adopt(newMenuItem);
+	};
+	
+	this.addBreak = function() {
+		newMenuItem = new Element('hr', {
+			'class': 'break'
+		});
+		
+		this.menuElement.adopt(newMenuItem);
+	};
+	
+	this.dispose = function() {
+		this.menuElement.dispose();
+	};
+	
+	this.menuElement = new Element('div', {
+		'class': 'dropMenu hidden',
+		'styles': {
+			'top': '0',
+			'left': '0'
+		}
+	});
+	
 	var bodyElement = $(document.body);
+	bodyElement.adopt(this.menuElement);
+};
+
+function contextMenuHide() {
+	if (openContextMenu != null) {
+        openContextMenu.hide();
+    }
+}
+
+function Notification(root) {
+	this.bodyElement = root;
 	notificationBox = new Element('div', {
 		'class': 'notifyBox hidden'
 	});
-	notification = new Element('div', {
+	notificationText = new Element('div', {
 		'class': 'notify'
 	});
-	notificationBox.adopt(notification);
-	bodyElement.adopt(notificationBox);
+	notificationBox.adopt(notificationText);
+	this.bodyElement.adopt(notificationBox);
 }
-
-window.addEvent('domready', bootScripts);
--- trunk/ctpweb/js/epg.js
+++ trunk/ctpweb/js/epg.js
@@ -8,101 +8,9 @@ var epgUpdater = null;
 var dayscrubElement = null;
 var epgPrecision = 60 * 10;
 
-var openContextMenu = null;
+var notification;
 
 var fits = 4;
-
-function ContextMenu(element) {
-	this.menuElement = null;
-	
-	this.attachmentElement = typeof(element) != 'undefined' ? element : null;
-	
-	this.show = function(x, y) {
-		if (this.attachmentElement != null) {
-			this.menuElement.style.left = (getPageX(this.attachmentElement) - 132) + "px";
-			this.menuElement.style.top = (getPageY(this.attachmentElement) + 23) + "px";
-		}
-		else {
-			this.menuElement.style.left = x + "px";
-			this.menuElement.style.top = y + "px";
-		}
-		
-		if (this.menuElement.hasClass('hidden')) {
-			this.menuElement.removeClass('hidden');
-		}
-		
-		openContextMenu = this;
-		
-		var bodyElement = $(document.body);
-		bodyElement.addEvent('click', function(event) {
-			if (openContextMenu != null) {
-				openContextMenu.hide();
-				event.stop();
-			}
-		});
-	};
-	
-	this.hide = function() {
-		bodyElement.removeEvent('click', openContextMenu.hide);
-		
-		if (!openContextMenu.menuElement.hasClass('hidden')) {
-			openContextMenu.menuElement.addClass('hidden');
-		}
-		
-		openContextMenu.dispose();
-		openContextMenu = null;
-	};
-	
-	this.addMenuItem = function(caption, callback, isDefault) {
-		isDefault = typeof(isDefault) != 'undefined' ? isDefault : false;
-		
-		if (isDefault) {
-			extraClass = ' default';
-		}
-		else {
-			extraClass = '';
-		}
-		
-		newMenuItem = new Element('div', {
-			'class': 'menuItem' + extraClass
-		});
-		
-		newMenuItem.innerHTML = caption;
-		
-		newMenuItem.addEvent('click', callback);
-		
-		this.menuElement.adopt(newMenuItem);
-	};
-	
-	this.addBreak = function() {
-		newMenuItem = new Element('hr', {
-			'class': 'break'
-		});
-		
-		this.menuElement.adopt(newMenuItem);
-	};
-	
-	this.dispose = function() {
-		this.menuElement.dispose();
-	};
-	
-	this.menuElement = new Element('div', {
-		'class': 'dropMenu hidden',
-		'styles': {
-			'top': '0',
-			'left': '0'
-		}
-	});
-	
-	var bodyElement = $(document.body);
-	bodyElement.adopt(this.menuElement);
-};
-
-function contextMenuHide() {
-	if (openContextMenu != null) {
-        openContextMenu.hide();
-    }
-}
 
 function openMiniWindow(url, name, width, height) {
 	var popupWindow = window.open(url, name, "width=" + width + ",height=" + height + ",menubar=no,location=no,resizable=yes,scrollbars=no,status=no,centerscreen=yes,directories=no");
@@ -356,7 +264,7 @@ function loadGuide(guide){
 	
 	epgUpdater = setTimeout(updateGuide, 1000 * epgPrecision);
 	
-	hideNotification();
+	notificationHide();
 }
 
 function padZeros(number) {
@@ -367,7 +275,7 @@ function padZeros(number) {
 }
 
 function updateGuide(){
-	showNotification("Loading...");
+	notificationShow("Loading...");
 	
 	var urlExploded = location.href.split('#');
 	var extUrl = "";
@@ -442,6 +350,8 @@ function bootScripts() {
 	
 	dayscrubElement = new Dayscrub($('daylist'), new Date().decrement('day', 7), 14);
 	dayscrubElement.refreshCurrent();
+	
+	notification = new Notification($(document.body));
 	
 	hasUrlChanged();
 	windowResize();
