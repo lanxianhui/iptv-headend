c15df39986dc5a283300bf2de225e7cb55e28e11
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-05-16 01:12:32 +0000

    Minor improvements, awaiting testing

--- npvrd/src/pl/lodz/p/cm/ctp/npvrd/FileSink.java
+++ npvrd/src/pl/lodz/p/cm/ctp/npvrd/FileSink.java
@@ -11,16 +11,30 @@ public class FileSink implements Sink {
 	private boolean error = false;
 	private boolean active = true;
 	
+	/**
+	 * Create a new FileSink
+	 * @param fileStream The file stream to base it on
+	 * @param begin The time (in milliseconds) at which to begin recording
+	 * @param end The time (in millisenconds) at which the recording should end
+	 */
 	FileSink(OutputStream fileStream, long begin, long end) {
 		this.fileStream = fileStream;
 		this.begin = begin;
 		this.end = end;
 	}
 	
+	/**
+	 * Get the set recording begin time
+	 * @return Begin time (in milliseconds) since Unix Epoch
+	 */
 	public long getBegin() {
 		return begin;
 	}
 
+	/**
+	 * Get the set recording end time
+	 * @return End time (in milliseconds) since Unix Epoch
+	 */
 	public long getEnd() {
 		return end;
 	}
