af6195da0f58c4d58ccad44eb5046da1275eb68a
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2010-06-02 16:34:42 +0000

    Cleaner now uses cron4j for scheduling.

--- trunk/npvrd/src/pl/lodz/p/cm/ctp/npvrd/Cleaner.java
+++ trunk/npvrd/src/pl/lodz/p/cm/ctp/npvrd/Cleaner.java
@@ -7,61 +7,43 @@ import pl.lodz.p.cm.ctp.dao.*;
 import pl.lodz.p.cm.ctp.dao.model.*;
 
 public class Cleaner implements Runnable {
-	public enum RunMode { RUN, STOP };
-	
-	private RunMode runMode = RunMode.RUN;
-	
 	private String logPrefix = "Cleaner: ";
+	
+	private List<TvChannel> tvChannels;
+	private TvChannelDAO tvChannelDAO;
+	private RecordingDAO recordingDAO;
+	private ProgramRecordingDAO programDvrScheduleDAO;
 	
 	public Cleaner() {
 		Npvrd.log(logPrefix + "Starting up");
-	}
-	
-	public RunMode getRunMode() {
-		return this.runMode;
-	}
-	
-	public void setRunMode(RunMode runModeIn) {
-		this.runMode = runModeIn;
+		DAOFactory dbase = DAOFactory.getInstance(Npvrd.config.database);
+		programDvrScheduleDAO = dbase.getProgramRecordingDAO();
+		tvChannelDAO = dbase.getTvChannelDAO();
+		recordingDAO = dbase.getRecordingDAO();
+		try {
+			tvChannels = tvChannelDAO.list();
+		} catch (DAOException e) {
+			Npvrd.error(logPrefix + "Unable to get channel list");
+		}
 	}
 
 	@Override
 	public void run() {
-		DAOFactory dbase = DAOFactory.getInstance(Npvrd.config.database);
-		ProgramRecordingDAO programDvrScheduleDAO = dbase.getProgramRecordingDAO();
-		TvChannelDAO tvChannelDAO = dbase.getTvChannelDAO();
-		RecordingDAO recordingDAO = dbase.getRecordingDAO();
-		List<TvChannel> tvChannels;
-		try {
-			tvChannels = tvChannelDAO.list();
-			
-			while (runMode.equals(RunMode.RUN)) {
-				int counter = 0;
-				for (TvChannel tc : tvChannels) {
-					try {
-						List<ProgramRecording> forDeletion = programDvrScheduleDAO.listOlderThanHours(tc.getId(), Npvrd.config.cleanerTolerance);
-						for (ProgramRecording cpr : forDeletion) {
-							File fileForDeletion = new File(Npvrd.config.recordings + cpr.recording.getFileName());
-							recordingDAO.delete(cpr.recording);
-							fileForDeletion.delete();
-							counter++;
-						}
-					} catch (DAOException e) {
-						Npvrd.error(logPrefix + "Problem working with the database in Cleaner");
-					}	
+		int counter = 0;
+		for (TvChannel tc : tvChannels) {
+			try {
+				List<ProgramRecording> forDeletion = programDvrScheduleDAO.listOlderThanHours(tc.getId(), Npvrd.config.cleanerTolerance);
+				for (ProgramRecording cpr : forDeletion) {
+					File fileForDeletion = new File(Npvrd.config.recordings + cpr.recording.getFileName());
+					recordingDAO.delete(cpr.recording);
+					fileForDeletion.delete();
+					counter++;
 				}
-				Npvrd.log(logPrefix + "Deleted " + counter + " recordings.");
-				
-				try {
-					Thread.sleep(Npvrd.config.cleanerResolution * 60 * 60 * 1000);
-				} catch (InterruptedException e) {
-					// Someone woke us up, perhaps to inform us about the new RunMode. No harm done
-					// In executing another loop.
-				}
-			}
-		} catch (DAOException e1) {
-			Npvrd.error(logPrefix + "Unable to get TvChannels!");
+			} catch (DAOException e) {
+				Npvrd.error(logPrefix + "Problem working with the database in Cleaner");
+			}	
 		}
+		Npvrd.log(logPrefix + "Deleted " + counter + " recordings.");
 	}
 
 }
