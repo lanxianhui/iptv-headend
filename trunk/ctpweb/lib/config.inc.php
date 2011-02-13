da878292ed3bc73db214263f2f3c0096bd334405
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2011-02-13 00:25:55 +0000

    Config file updated.

--- trunk/ctpweb/lib/config.inc.php
+++ trunk/ctpweb/lib/config.inc.php
@@ -1,13 +1,25 @@
 <?php
+	/*
+		This is the config file for the CTPweb component
+		
+		Please, before any other setup, see the "salt.inc.php" to set up the password salt.
+	*/
+
 	$config["database"]["host"] = "localhost";
 	$config["database"]["name"] = "cm_iptv";
 	$config["database"]["username"] = "iptv-test";
 	$config["database"]["password"] = "iptv-test";
 	
+	// The root url of the appliaction
 	$config["app"]["url"] = "http://vegas.cm.p.lodz.pl/ctpweb/";
 	
-	$config["pvr"]["url"] = "http://vegas.cm.p.lodz.pl/pvr/";
+	// The root url of the folder in which nPVRd stores all the recordings: can be HTTP
+	// or RTSP, if you have a RTSP server installed.
+	$config["pvr"]["url"] = "http://vegas.cm.p.lodz.pl/pvr/"; 
+	// The root url of the folder in which nPVRd recordings are stored: HTTP only.
 	$config["pvr"]["downloadUrl"] = "http://vegas.cm.p.lodz.pl/pvr/";
-	$config["pvr"]["userQuota"] = 600; // 10h of recording by default
-	$config["pvr"]["maxHold"] = 60 * 24 * 30; // 30 days of holding possible 
+	// 10h of recording by default, will be used for every new user
+	$config["pvr"]["userQuota"] = 600; 
+	// 30 days of holding possible, has to be equal to what nPVRd has in it's config file.
+	$config["pvr"]["maxHold"] = 60 * 24 * 30; 
 ?>
