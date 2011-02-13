2c41c4d1940a4bb216cd948254f17a2116f1195f
jan.starzak <jan.starzak@92d7ae50-0844-11df-830e-35ef22166b74>
2011-02-13 00:13:18 +0000

    Latest version, some fixes in Cleaner. Still to come: Settings and improvments to My List. Also: News component.

--- trunk/npvrd/src/pl/lodz/p/cm/ctp/npvrd/Cleaner.java
+++ trunk/npvrd/src/pl/lodz/p/cm/ctp/npvrd/Cleaner.java
@@ -44,45 +44,67 @@ public class Cleaner implements Runnable {
 				
 				for (ProgramRecording cpr : forDeletion) {
 					boolean deletable = true;
-					List<UserRecording> subscribers = userRecordingDAO.listByRecordingId(cpr.getRecording().getId());
-					for (UserRecording sub : subscribers) {
-						Calendar cal = Calendar.getInstance();
-						if (sub.getCreatedOn().before(cpr.getProgram().getEnd())) {
-							//Timestamp EndPlusHold = (Timestamp)cpr.getProgram().getEnd().clone();
-							Date endPlusHold = new Date(cpr.getProgram().getEnd().getTime());
-							cal.setTime(endPlusHold);
-							cal.add(Calendar.HOUR_OF_DAY, maxHold);
-							if (cal.after(now)) {
-								deletable = false;
+					
+					try {
+						List<UserRecording> subscribers = userRecordingDAO.listByRecordingId(cpr.getRecording().getId());
+						for (UserRecording sub : subscribers) {
+							Calendar cal = Calendar.getInstance();
+							if (sub.getCreatedOn().before(cpr.getProgram().getEnd())) {
+								//Timestamp EndPlusHold = (Timestamp)cpr.getProgram().getEnd().clone();
+								Date endPlusHold = new Date(cpr.getProgram().getEnd().getTime());
+								cal.setTime(endPlusHold);
+								cal.add(Calendar.HOUR_OF_DAY, maxHold);
+								if (cal.after(now)) {
+									deletable = false;
+								} else {
+									subscriptionsToLetGo.add(sub);
+								}
 							} else {
-								subscriptionsToLetGo.add(sub);
-							}
-						} else {
-							Date createdOnPlusHold = (Date)sub.getCreatedOn().clone();
-							cal.setTime(createdOnPlusHold);
-							cal.add(Calendar.HOUR_OF_DAY, maxHold);
-							if (cal.after(now)) {
-								deletable = false;
-							} else {
-								subscriptionsToLetGo.add(sub);
+								Date createdOnPlusHold = (Date)sub.getCreatedOn().clone();
+								cal.setTime(createdOnPlusHold);
+								cal.add(Calendar.HOUR_OF_DAY, maxHold);
+								if (cal.after(now)) {
+									deletable = false;
+								} else {
+									subscriptionsToLetGo.add(sub);
+								}
 							}
 						}
+						for (UserRecording sub : subscriptionsToLetGo) {
+							userRecordingDAO.delete(sub);
+						}
+					} catch (DAOException subException) {
+						Npvrd.error(logPrefix + "Exception in Cleaner, iterating through subscriptions. Message: " + subException.getMessage());
+						Npvrd.error(logPrefix + "Stack trace: ");
+						
+						// TODO: Debug only
+						subException.printStackTrace();
 					}
-					for (UserRecording sub : subscriptionsToLetGo) {
-						userRecordingDAO.delete(sub);
-					}
+					
 					if (deletable) {
-						File fileForDeletion = new File(Npvrd.config.recordings + cpr.getRecording().getFileName());
-						recordingDAO.delete(cpr.getRecording());
-						boolean result = fileForDeletion.delete();
-						if (!result) {
-							Npvrd.error(logPrefix + "Could not delete file " + cpr.getRecording().getFileName() + ". Dropped nonetheless.");
+						try {
+							File fileForDeletion = new File(Npvrd.config.recordings + cpr.getRecording().getFileName());
+							recordingDAO.delete(cpr.getRecording());
+							boolean result = fileForDeletion.delete();
+							if (!result) {
+								Npvrd.error(logPrefix + "Could not delete file " + cpr.getRecording().getFileName() + " from filesystem.");
+							}
+							counter++;
+						} catch (DAOException fileException) {
+							Npvrd.error(logPrefix + "Exception in Cleaner, deleting file. Message: " + fileException.getMessage());
+							Npvrd.error(logPrefix + "Stack trace: ");
+
+							// TODO: Debug only
+							fileException.printStackTrace();
 						}
-						counter++;
 					}
 				}
 			} catch (DAOException e) {
-				Npvrd.error(logPrefix + "Problem working with the database in Cleaner. Message: " + e.getMessage());
+				Npvrd.error(logPrefix + "Exception in Cleaner, interating through channels. Message: " + e.getMessage());
+				Npvrd.error(logPrefix + "Stack trace: ");
+				
+				// TODO: Debug only
+				e.printStackTrace();
 			}	
 		}
 		Npvrd.log(logPrefix + "Deleted " + counter + " recordings.");
